9e97f2446f7a8fbca446395dd4e4a3d488eb449a
Юрий Савенков <yura-savenkov1@yandex.ru>
2020-10-24 21:44:55 +0300

    Search Component

---
+++ js/SearchMovies.js
@@ -0,0 +1,11 @@
+function SearchMovies() {
+    return (
+        <form className="form">
+            <label htmlFor="query" className="label">Movie Name</label>
+            <input className="input" type="text" name="query" placeholder="Type in a movie" />
+            <button className="button" type="submit" >Search</button>
+        </form>
+    )
+}
+
+export default SearchMovies;
--- js/index.js
+++ js/index.js
@@ -1,24 +1,9 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
-// class Main extends Component {
-//     render() {
-//       return (
-//         <h1>Hello world!</h1>
-//       );
-//     }
-// }
-  
-// ReactDOM.render(<Main />, document.getElementById('root'))
-
 class Main extends React.Component {
-  constructor() {
-      super()      
-  }
   render() {
     return (
       <div className="container">
         <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
       </div>
   )
   } 
