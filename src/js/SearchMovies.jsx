e7b097680f067dae88edbbacb61564c4ebd582a3
Юрий Савенков <yura-savenkov1@yandex.ru>
2020-10-28 11:15:32 +0300

    displaying movie data

--- src/js/SearchMovies.jsx
+++ src/js/SearchMovies.jsx
@@ -36,9 +36,19 @@ function SearchMovies() {
             <button className="button" type="submit" >Search</button>
         </form>
         <div className="card-list">
-            {movies.map(movie => (
-                <div className="card">
-                    
+            {movies.filter(movie => movie.poster_path).map(movie => (
+                <div className="card" key={movie.id}>
+                    <img 
+                        className="card__image" 
+                        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                        alt={movie.title + ' poster'}
+                    />
+                    <div className="card__content">
+                        <h3 className="card__title">{movie.title}</h3>
+                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                        <p><small>RATING: {movie.vote_avarage}</small></p>
+                        <p className="card__description">{movie.overview}</p>
+                    </div>
                 </div>
             ))}
         </div>
